8c622097738ff820f1e569aa27bc06dcfdc13829
Sigurd Drivstuen <sigdriv06@gmail.com>
2023-10-02 11:33:19 +0200

    Removed commented code

--- JS/script.js
+++ JS/script.js
@@ -257,101 +257,101 @@ console.log(player1, player2)
 // addPoint(1)
 console.log(player1, player2)
 function updateScore(countPlayers = ""){
-    // fetch('http://pingpong.aastum.no/api/send/player1', {
-    // method: 'GET',
-    // headers: {
-    //     'Content-Type': 'application/json'
-    // }
-    // })
-    // .then(response => {
-    //     if (response.ok) {
-    //         return response.json(); // Parse the JSON response
-    //     } else {
-    //         console.error('Failed to fetch data');
-    //     }
-    // })
-    // .then(data => {
-    //     // console.log('Data received from the server:', data.id, data.score);
-    //     // Use the data in your web application
-    //     player1 = data
-    //     player1.password = localStorage.getItem("password")
-    //     localStorage.setItem("player1", JSON.stringify(player1))
-    // })
-    // .catch(error => {
-    //     console.error('Error:', error);
-    // });
+    fetch('http://pingpong.aastum.no/api/send/player1', {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json(); // Parse the JSON response
+        } else {
+            console.error('Failed to fetch data');
+        }
+    })
+    .then(data => {
+        // console.log('Data received from the server:', data.id, data.score);
+        // Use the data in your web application
+        player1 = data
+        player1.password = localStorage.getItem("password")
+        localStorage.setItem("player1", JSON.stringify(player1))
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 
-    // fetch('http://pingpong.aastum.no/api/send/player2', {
-    //     method: 'GET',
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     }
-    // })
-    // .then(response => {
-    //     if (response.ok) {
-    //         return response.json(); // Parse the JSON response
-    //     } else {
-    //         console.error('Failed to fetch data');
-    //     }
-    // })
-    // .then(data => {
-    //     // console.log('Data received from the server:', data.id, data.score);
-    //     // Use the data in your web application
-    //     player2 = data
-    //     player2.password = localStorage.getItem("password")
-    //     localStorage.setItem("player2", JSON.stringify(player2))
-    // })
-    // .catch(error => {
-    //     console.error('Error:', error);
-    // });
+    fetch('http://pingpong.aastum.no/api/send/player2', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json(); // Parse the JSON response
+        } else {
+            console.error('Failed to fetch data');
+        }
+    })
+    .then(data => {
+        // console.log('Data received from the server:', data.id, data.score);
+        // Use the data in your web application
+        player2 = data
+        player2.password = localStorage.getItem("password")
+        localStorage.setItem("player2", JSON.stringify(player2))
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 
-    // fetch('http://pingpong.aastum.no/api/send/player3', {
-    // method: 'GET',
-    // headers: {
-    //     'Content-Type': 'application/json'
-    // }
-    // })
-    // .then(response => {
-    //     if (response.ok) {
-    //         return response.json(); // Parse the JSON response
-    //     } else {
-    //         console.error('Failed to fetch data');
-    //     }
-    // })
-    // .then(data => {
-    //     // console.log('Data received from the server:', data.id, data.score);
-    //     // Use the data in your web application
-    //     player3 = data
-    //     player3.password = localStorage.getItem("password")
-    //     localStorage.setItem("player3", JSON.stringify(player3))
-    // })
-    // .catch(error => {
-    //     console.error('Error:', error);
-    // });
+    fetch('http://pingpong.aastum.no/api/send/player3', {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json(); // Parse the JSON response
+        } else {
+            console.error('Failed to fetch data');
+        }
+    })
+    .then(data => {
+        // console.log('Data received from the server:', data.id, data.score);
+        // Use the data in your web application
+        player3 = data
+        player3.password = localStorage.getItem("password")
+        localStorage.setItem("player3", JSON.stringify(player3))
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 
-    // fetch('http://pingpong.aastum.no/api/send/player4', {
-    //     method: 'GET',
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     }
-    // })
-    // .then(response => {
-    //     if (response.ok) {
-    //         return response.json(); // Parse the JSON response
-    //     } else {
-    //         console.error('Failed to fetch data');
-    //     }
-    // })
-    // .then(data => {
-    //     // console.log('Data received from the server:', data.id, data.score);
-    //     // Use the data in your web application
-    //     player4 = data
-    //     player4.password = localStorage.getItem("password")
-    //     localStorage.setItem("player4", JSON.stringify(player4))
-    // })
-    // .catch(error => {
-    //     console.error('Error:', error);
-    // });
+    fetch('http://pingpong.aastum.no/api/send/player4', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json(); // Parse the JSON response
+        } else {
+            console.error('Failed to fetch data');
+        }
+    })
+    .then(data => {
+        // console.log('Data received from the server:', data.id, data.score);
+        // Use the data in your web application
+        player4 = data
+        player4.password = localStorage.getItem("password")
+        localStorage.setItem("player4", JSON.stringify(player4))
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 
 
     if (!switchedSidesG1 && countPlayers == 2){
